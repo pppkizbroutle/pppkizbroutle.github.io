49d166b71dcd7ba859984e41974d3802ff3e5c99
pppkizbroutle <erikrangel.014@ciencias.unam.mx>
2022-08-18 13:14:47 -0500

    Página algoritmos terminada

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.simplecss.org/simple.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
